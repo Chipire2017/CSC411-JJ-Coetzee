84723296fa9a87b34e39e24c132d5a0f8e1bf7be
Carl Sandrock <carl.sandrock@rockconsulting.co.za>
2013-02-25 10:59:29 +0200

    CODE BROKEN Follow python conventions for for loops

--- Networkx.py
+++ Networkx.py
@@ -15,32 +15,30 @@ from sympy.parsing.sympy_parser import parse_expr
 #-------------------------------------------------------------------------
 # Extraction of Equations and Graph Creation
 eqns = [line.strip() for line in open('eqns.txt')]
-unkn = symbols([line.strip() for line in open('Unknowns.txt')])
-eqn_numbering = {}
+unkns = symbols([line.strip() for line in open('Unknowns.txt')])
 edge_data = []
 unknown = ''
-eqn = ''
+
 listunkn = {}
 #print unkn
 #print eqns
 
 G = nx.MultiDiGraph()
 
-for i in range(0,len(unkn)):
+for unkn in unkns:
     for k in range(0,len(eqns)):
-        eqn_numbering[k+1] = eqns[k]
         eqn = sy.parsing.sympy_parser.parse_expr(eqns[k])
-        listunkn[k+1] = str(eqn(k).args)
-        if unkn[i] in eqn:
+        #listunkn[k+1] = str(eqn(k).args)
+        if unkn in eqn:
             curr_node = k+1
             for j in range(k+1,len(eqns)):
-                eqn2 = sy.parsing.sympy_parser.parse_expr(eqns[j]) 
-                if unkn[i] in eqn2:
+                eqn2 = sy.parsing.sympy_parser.parse_expr(eqn)
+                if unkn in eqn2:
                     next_node = j+1
                     if G.has_edge(curr_node,next_node):
-                        G.add_edge(next_node,curr_node,weight=1,label=str(unkn[i]))
+                        G.add_edge(next_node,curr_node,weight=1,label=str(unkn))
                     else:
-                        G.add_edge(curr_node,next_node,weight=0.5,label=str(unkn[i]))
+                        G.add_edge(curr_node,next_node,weight=0.5,label=str(unkn))
 
 
 #-------------------------------------------------------------------
@@ -54,24 +52,15 @@ teller = 0
 Tarjan.reverse()
 eqan = []
 
-for i in range(0,len(Tarjan)):
-    curr = Tarjan[i]
-    if len(curr) == 1:
-        now = curr[0]
-        curr_eqn = eqn_numbering.get(curr[0])       
-        eqan = sy.sympify(curr_eqn)
-        soln = sy.solve(eqan,unkn)
-        
-    elif len(curr) > 1:
-        for k in range(0,len(curr)):
-            now = curr[k]
-            simu.append(eqn_numbering.get(now)) 
-        eqan = sy.sympify(simu)
-        soln = sy.solve(eqan,unkn)
+for curr in Tarjan:
+    for now in curr:
+        simu.append(eqns[now])
+    eqan = sy.sympify(simu)
+    soln = sy.solve(eqan,unkn)
     print soln
 
 print Tarjan
-print listunkn
+#print listunkn
 
 #--------------------------------------------------------------------
 # Drawing of Graph
