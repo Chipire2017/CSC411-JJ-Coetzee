46b2f68aced0310809609ebe4aaaeeef2c242f6e
Johan Coetzee <u10017888@tuks.co.za>
2013-03-04 15:38:09 +0200

    SOLVER Working with changes to parsing, symbols
    All code not needed has also been removed

--- Networkx.py
+++ Networkx.py
@@ -1,48 +1,73 @@
+<<<<<<< HEAD
 try:
     import matplotlib.pyplot as plt
 except:
     raise
 
+=======
+#2013-02-11
+#2013-02-16
+
+import matplotlib.pyplot as plt
+>>>>>>> origin/merge
 import networkx as nx
 import sympy as sy
-from sympy import *
 from sympy.parsing.sympy_parser import parse_expr
-
 
 #-------------------------------------------------------------------------
 # Extraction of Equations and Graph Creation
+<<<<<<< HEAD
 eqns = [line.strip() for line in open('eqns.txt')]
 unkns = symbols([line.strip() for line in open('Unknowns.txt')])
+=======
+eqns = [sy.parsing.sympy_parser.parse_expr(line) for line in open('eqns.txt')]
+unkns = [x for x in set.union(*(eq.atoms() for eq in eqns)) if x.is_Symbol]
+edge_data = []
+unknown = ''
+
+listunkn = {}
+#print unkn
+#print eqns
+>>>>>>> origin/merge
 
 G = nx.MultiDiGraph()
 
-for unkn in unkns:
-    for k in range(0,len(eqns)):
-        eqn = sy.parsing.sympy_parser.parse_expr(eqns[k])
-        if unkn in eqn:
-            curr_node = k
-            for j in range(k,len(eqns)):
-                eqn2 = sy.parsing.sympy_parser.parse_expr(eqns[j])
-                if unkn in eqn2:
-                    next_node = j
-                    if G.has_edge(curr_node,next_node):
-                        G.add_edge(next_node,curr_node,weight=1,label=str(unkn))
+for unknown in unkns:
+    for curr_node, eqn in enumerate(eqns):
+        if unknown in eqn:
+            for next_node in range(curr_node, len(eqns)):
+                eqn2 = eqns[next_node]
+                if unknown in eqn2:
+                    if G.has_edge(curr_node, next_node):
+                        weight = 1
                     else:
-                        G.add_edge(curr_node,next_node,weight=0.5,label=str(unkn))
+                        weight = 0.5
+                    G.add_edge(next_node, curr_node, weight=weight, label=str(unknown))
 
 
 #-------------------------------------------------------------------
 # Tarjan Algorithm and Symbolic Solving of Equations
 
 Tarjan = nx.strongly_connected_components(G)
+<<<<<<< HEAD
 
 simu = []
+=======
+soln = 0
+simu = []
+
+>>>>>>> origin/merge
 Tarjan.reverse()
 
 for curr in Tarjan:
     for now in curr:
         simu.append(eqns[now])
+<<<<<<< HEAD
     soln = sy.solve(simu,unkns)
+=======
+    eqan = sy.sympify(simu)
+    soln = sy.solve(eqan, unkns)
+>>>>>>> origin/merge
     print soln
 
 print Tarjan
@@ -53,14 +78,18 @@ print Tarjan
 
 pos = nx.spring_layout(G)
 sypy = sy.sympify(eqns)
-sol = sy.solve(sypy,unkns)
+sol = sy.solve(sypy, unkns)
 
 print sol
 
+<<<<<<< HEAD
 nx.draw_networkx(G,pos)
 nx.draw_networkx_edge_labels(G,pos)
+=======
+nx.draw_networkx(G, pos)
+nx.draw_networkx_edge_labels(G, pos)
+#nx.draw_spring(G)
+>>>>>>> origin/merge
 
 plt.axis('off')
 plt.show()
-
-
